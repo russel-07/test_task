bd8c88fcc367aa836e2b03be4fbd248fd67acfd3
russel-07 <russel170790@gmail.com>
2023-12-26 13:06:26 +0300

    main update

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from question_1 import isEven
-from question_2 import ArrayRingBuffer, LinkedListRingBuffer
+from question_1 import check_isEven_func
+from question_2 import check_ArrayQueue_class, check_LinkedListQueue_class
+from question_3 import check_sort_func
 
-a = ArrayRingBuffer(5)
-b = LinkedListRingBuffer
 
 if __name__ == '__main__':
-    assert isEven(0) == True
-    assert isEven(1) == False
-    assert isEven(12) == True
-    assert isEven(12345) == False
-    assert a.push() == 'Buffer is empty'
+    check_isEven_func()
+    check_ArrayQueue_class()
+    check_LinkedListQueue_class()
+    check_sort_func()
